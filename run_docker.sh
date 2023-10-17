9d48496cd716376afa5d75545ef17692bc62f742
Renan Siqueira Antonio <renan.siqu@gmail.com>
2023-10-17 16:39:10 -0300

    feat: Docker container integration with Nvidia CUDA for execution using GPU

--- run_docker.sh
+++ run_docker.sh
@@ -4,5 +4,7 @@
 win_path=$(pwd -W)
 lowercase_path=$(echo $win_path | tr '[:upper:]' '[:lower:]' | sed 's|\\|/|g')
 
-# Use the transformed path to run your Docker container, for example:
-docker run --gpus all -it --rm -v "/$lowercase_path":/app face_recognition_gpu bash
+# Use the transformed path to run your Docker container with specified commands
+docker run --gpus all -it --rm -v "/$lowercase_path":/app face_recognition_gpu bash -c "\
+    if [ ! -d '/app/.venv' ]; then mv /tmp/.venv_temp /app/.venv; fi && \
+    bash"
